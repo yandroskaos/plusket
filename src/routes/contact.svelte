83ab0b02f40beef4a210b969aa59253ef3cb1ac9
Pablo Bravo <yandroskaos@gmail.com>
2020-07-09 22:30:22 +0200

    I

---
+++ src/routes/contact.svelte
@@ -0,0 +1,21 @@
+<Button on:click={()=> alert('Clicked!')}>Just a Button</Button>
+<Button variant="raised"><Label>Raised Button, Using a Label</Label></Button>
+<Button some-arbitrary-prop="placed on the actual button">Button</Button>
+
+<Fab on:click={()=> alert('Clicked!')} extended>
+	<Icon class="material-icons" style="margin-right: 12px;">favorite</Icon>
+	<Label>Extended FAB</Label>
+</Fab>
+
+<Textfield bind:value={superText} label="Super Text" input$aria-controls="super-helper" input$aria-describedby="super-helper" />
+<HelperText id="super-helper">What you put in this box will become super!</HelperText>
+
+<script>
+  import Button from "@smui/button";
+  import Fab from "@smui/fab";
+  import Textfield from "@smui/textfield";
+  import HelperText from "@smui/textfield/helper-text";
+  import { Label, Icon } from "@smui/common";
+
+  let superText = "";
+</script>
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
-const webpack = require('webpack');
-const path = require('path');
-const config = require('sapper/config/webpack.js');
-const pkg = require('./package.json');
+const webpack = require("webpack");
+const path = require("path");
+const config = require("sapper/config/webpack.js");
+const pkg = require("./package.json");
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 const mode = process.env.NODE_ENV;
-const dev = mode === 'development';
+const dev = mode === "development";
 
-const alias = { svelte: path.resolve('node_modules', 'svelte') };
-const extensions = ['.mjs', '.js', '.json', '.svelte', '.html'];
-const mainFields = ['svelte', 'module', 'browser', 'main'];
+const alias = { svelte: path.resolve("node_modules", "svelte") };
+const extensions = [".mjs", ".js", ".json", ".svelte", ".html"];
+const mainFields = ["svelte", "module", "browser", "main"];
 
 module.exports = {
   client: {
@@ -20,58 +23,86 @@ module.exports = {
         {
           test: /\.(svelte|html)$/,
           use: {
-            loader: 'svelte-loader',
+            loader: "svelte-loader",
             options: {
               dev,
               hydratable: true,
-              hotReload: false, // pending https://github.com/sveltejs/svelte/issues/2377
-            },
-          },
+              hotReload: false // pending https://github.com/sveltejs/svelte/issues/2377
+            }
+          }
         },
-      ],
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+            "style-loader",
+            MiniCssExtractPlugin.loader,
+            "css-loader",
+            {
+              loader: "sass-loader",
+              options: {
+                sassOptions: {
+                  includePaths: ["./theme", "./node_modules"]
+                }
+              }
+            }
+          ]
+        }
+      ]
     },
     mode,
     plugins: [
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[name].[id].css"
+      }),
+      new OptimizeCssAssetsPlugin({
+        assetNameRegExp: /\.css$/g,
+        cssProcessor: require("cssnano"),
+        cssProcessorPluginOptions: {
+          preset: ["default", { discardComments: { removeAll: true } }]
+        },
+        canPrint: true
+      }),
       // pending https://github.com/sveltejs/svelte/issues/2377
       // dev && new webpack.HotModuleReplacementPlugin(),
       new webpack.DefinePlugin({
-        'process.browser': true,
-        'process.env.NODE_ENV': JSON.stringify(mode),
-      }),
+        "process.browser": true,
+        "process.env.NODE_ENV": JSON.stringify(mode)
+      })
     ].filter(Boolean),
-    devtool: dev && 'inline-source-map',
+    devtool: dev && "inline-source-map"
   },
 
   server: {
     entry: config.server.entry(),
     output: config.server.output(),
-    target: 'node',
+    target: "node",
     resolve: { alias, extensions, mainFields },
-    externals: Object.keys(pkg.dependencies).concat('encoding'),
+    externals: Object.keys(pkg.dependencies).concat("encoding"),
     module: {
       rules: [
         {
           test: /\.(svelte|html)$/,
           use: {
-            loader: 'svelte-loader',
+            loader: "svelte-loader",
             options: {
               css: false,
-              generate: 'ssr',
-              dev,
-            },
-          },
-        },
-      ],
+              generate: "ssr",
+              dev
+            }
+          }
+        }
+      ]
     },
     mode: process.env.NODE_ENV,
     performance: {
-      hints: false, // it doesn't matter if server.js is large
-    },
+      hints: false // it doesn't matter if server.js is large
+    }
   },
 
   serviceworker: {
     entry: config.serviceworker.entry(),
     output: config.serviceworker.output(),
-    mode: process.env.NODE_ENV,
-  },
+    mode: process.env.NODE_ENV
+  }
 };
